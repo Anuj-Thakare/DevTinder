8ccc211e4898b422e43533dd235d17fed4c93a5b
Anuj Thakare <anujthakare19@gmail.com>
2026-06-11 22:54:12 +0530

    Exploring GET, POST, PATCH and DELETE methods

--- src/app.js
+++ src/app.js
@@ -2,17 +2,31 @@ const express = require("express"); //Importing a express
 
 const app = express(); //Creating a expressjs application or instance of expressjs application
 
-app.use("/hello", (req, res) => {
-    res.send("Hello hello hello");
-}); //Handling a incomming request or listing to request
+//This will handle only get API calls
+app.get("/user", (req, res) => {
+    res.send("This is the GET API Call");
+});
 
+//This will handle only post API calls
+app.post("/user", (req, res) => {
+    res.send("This is the POST API Call");
+});
+
+//This will handle only patch API call
+app.patch("/user", (req, res) => {
+    res.send("This is the PATCH API Call");
+});
+
+//This will handle only DELETE API Call
+app.delete("/user", (req, res) => {
+    res.send("This is the DELETE API Call");
+});
+
+//This will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
     res.send("Hello from the server");
 }); //Handling a incomming request or listing to request
 
-app.use("/", (req, res) => {
-    res.send("Server Created and Running");
-}); //Handling a incomming request or listing to request
 
 app.listen(3000, () => {
     console.log("Server is created on port 3000....");
